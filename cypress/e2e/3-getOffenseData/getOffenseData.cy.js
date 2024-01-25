507e8feee90990819e4f42b1ba3523663d5118dd
Alejandro F <afuste7@gmail.com>
2024-01-25 13:33:16 -0500

    added code to get offense data and added team grading function in new util file

---
+++ cypress/e2e/3-getOffenseData/getOffenseData.cy.js
@@ -0,0 +1,70 @@
+import percentile from "../../../utils/calculations/percentile";
+import letterGrade from "../../../utils/calculations/letterGrade";
+
+describe("get each team's offensive data for the 2023 season and write to json file", () => {
+  let resultsMap = new Map();
+
+  it("gets offense data and assigns grades", () => {
+    // Go to website and verify
+    cy.visit(
+      "https://www.covers.com/sport/football/nfl/statistics/team-offense/2023-2024"
+    );
+
+    cy.location("href").should(
+      "eq",
+      "https://www.covers.com/sport/football/nfl/statistics/team-offense/2023-2024"
+    );
+
+    // Check if it's the right data and year
+    cy.get("h1").should("have.text", "NFL Stats - Team Offense 2023-2024");
+
+    // cy.get(".covers-CoversMatchups-subH1").should("have.text", "2023-2024");
+
+    // Click on season and points per game
+    cy.get(".covers-DataPages-tabButtonContainer > :nth-child(1) > a").click();
+
+    cy.get("#RegularSeason > thead > tr > th:nth-child(10)").click();
+
+    cy.get("#RegularSeason > tbody > tr").as("tableRows");
+
+    let total = 32;
+
+    cy.get("@tableRows").should("have.length", total);
+
+    // loop through table
+    cy.get("@tableRows").each(($tr, index, $lis) => {
+      const rowElement = $tr.get(0);
+      const cells = rowElement.cells;
+
+      let name = cells[0].innerText.replace(/\s/, "");
+      let rank = index + 1;
+      let percentileValue = percentile(total, rank);
+      let grade = letterGrade(percentileValue);
+
+      let data = {
+        name: name,
+        offense: {
+          pointsPerGame: parseFloat(cells[9].innerText),
+          QBR: parseFloat(cells[5].innerText),
+          passPerGame: parseFloat(cells[4].innerText),
+          rushPerGame: parseFloat(cells[2].innerText),
+          yardsPerGame: parseFloat(cells[1].innerText),
+          rank: rank,
+          percentile: percentileValue,
+          grade: grade,
+        },
+      };
+
+      resultsMap.set(name, data);
+      console.log(resultsMap);
+    });
+  });
+
+  it("write data to JSON file", () => {
+    let results = Object.fromEntries(resultsMap);
+    let jsonString = JSON.stringify(results, null, " ");
+    console.log(jsonString);
+
+    cy.writeFile("./data/offense/offenseData.json", jsonString);
+  });
+});
---
+++ utils/calculations/teamLetterGrade.js
@@ -0,0 +1,24 @@
+function teamLetterGrade(rank) {
+  if (rank <= 3) {
+    return { letter: "A+", description: "Elite" };
+  } else if (rank >= 4 && rank <= 6) {
+    return { letter: "A", description: "Great" };
+  } else if (rank >= 7 && rank <= 9) {
+    return { letter: "B+", description: "Good" };
+  } else if (rank >= 10 && rank <= 12) {
+    return { letter: "B", description: "Above Average" };
+  } else if (rank >= 13 && rank <= 20) {
+    return { letter: "C", description: "Average" };
+  } else if (rank >= 21 && rank <= 26) {
+    return { letter: "D", description: "Below Average" };
+  } else if (rank >= 27) {
+    return { letter: "F", description: "Not Good Enough" };
+  } else {
+    return {
+      letter: "N/A",
+      description: "Oops, something went wrong..could not calculate grade",
+    };
+  }
+}
+
+module.exports = teamLetterGrade;
