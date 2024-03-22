7cbf1f86a399fb6b038fe77bb2a3a3182ad380aa
Alejandro F <afuste7@gmail.com>
2024-03-22 19:23:44 -0400

    changed code for format name and added a test for it...updated json files to get changes to St.Brown grade

---
+++ test/utils/formatName.test.js
@@ -0,0 +1,16 @@
+// formatName.test.js
+const formatName = require("../../utils/formatName");
+
+describe("formatName file positive tests", () => {
+  test("removes suffix from a name", () => {
+    expect(formatName("Patrick Mahomes II")).toBe("Patrick Mahomes");
+    expect(formatName("Tom Brady Jr.")).toBe("Tom Brady");
+    expect(formatName("George Foreman III")).toBe("George Foreman");
+  });
+
+  test("does not modify name without suffix", () => {
+    expect(formatName("Michael Jordan")).toBe("Michael Jordan");
+    expect(formatName("LeBron James")).toBe("LeBron James");
+    expect(formatName("Amon-Ra St. Brown")).toBe("Amon-Ra St. Brown");
+  });
+});
